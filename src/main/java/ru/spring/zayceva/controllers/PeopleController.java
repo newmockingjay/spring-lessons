611788f3405c86d16a740946e1230e99b6d20135
Дарья Зайцева <new1mockingjay@mail.ru>
2024-03-05 20:59:57 +0300

    + create операция

--- src/main/java/ru/spring/zayceva/controllers/PeopleController.java
+++ src/main/java/ru/spring/zayceva/controllers/PeopleController.java
@@ -3,10 +3,9 @@ package ru.spring.zayceva.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.spring.zayceva.dao.PersonDAO;
+import ru.spring.zayceva.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -32,5 +31,19 @@ public class PeopleController {
         // получим одного человека по id и предадим на отображение
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+
+    @GetMapping("/new")
+    public String newPerson(Model model){
+        model.addAttribute("person", new Person()); //в th:object - этот person $ и его поля th:field *
+
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person){
+        personDAO.save(person);
+
+        return "redirect:/people";
     }
 }
