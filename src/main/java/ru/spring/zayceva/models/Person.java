c535cb67b8e315355f326e84080b4938213b8a22
Дарья Зайцева <new1mockingjay@mail.ru>
2024-03-06 21:27:20 +0300

    mini-changes

--- src/main/java/ru/spring/zayceva/models/Person.java
+++ src/main/java/ru/spring/zayceva/models/Person.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
 public class Person {
+    @NotEmpty
     private int id;
     @NotEmpty(message = "Name should not be empty")
     @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
